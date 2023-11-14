9db5100dfe03063cf421e9509c67b3ab9e84ebaf
woondo kwak <woondokwak@woondos-MacBook-Air.local>
2023-11-14 13:40:34 +0900

    초기 세팅 및 DayCout 추가

---
+++ src/components/DayCount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DayCount() {
+    return (
+        <section id="time-container">
+            <p className="countdown">d-11</p>
+        </section>
+    );
+}
+
+export default DayCount;
